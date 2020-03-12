c61db03ff87ed76760748f232669e8399f1feaea
Jet Li <jing.i.qin@icloud.com>
2020-03-12 15:12:04 +0800

    Test version param

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,9 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const version: string = core.getInput('version')
+    core.info(`Got version: ${version}`)
   } catch (error) {
     core.setFailed(error.message)
   }
