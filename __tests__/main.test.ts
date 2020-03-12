4a38c2ea289cc4017b2de3d01e45207aa3e8a408
Jet Li <jing.i.qin@icloud.com>
2020-03-12 19:12:18 +0800

    Fix tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,24 +1,31 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+import * as os from 'os'
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+test('test runs with a fixed version 0.9.1', () => {
+  process.env['RUNNER_TEMP'] = os.tmpdir()
+  process.env['INPUT_VERSION'] = '0.9.1'
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execSync(`node ${ip}`, options).toString())
+})
+
+test('test runs with the latest version ', () => {
+  process.env['RUNNER_TEMP'] = os.tmpdir()
+  process.env['INPUT_VERSION'] = 'latest'
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execSync(`node ${ip}`, options).toString())
+})
+
+test('test runs without version defined', () => {
+  process.env['RUNNER_TEMP'] = os.tmpdir()
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: process.env
